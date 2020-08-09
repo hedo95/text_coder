5ce219f914aa33d3ff1c7cb8fcff5b0376ffa672
hedo95 <44504802+hedo95@users.noreply.github.com>
2020-08-09 19:00:03 +0200

    Update decoder.py

--- lib/decoder.py
+++ lib/decoder.py
@@ -1,15 +1,19 @@
-class Decoder:
-
-    def __init__(self, bitstream, tree):
-        self.bitstream = bitstream
-        self.tree = dict((value,key) for key,value in tree.items())
-    
-    def text(self):
-        codeword = ''
-        text = ''
-        for bit in self.bitstream:
-            codeword += bit
-            if codeword in self.tree.keys():
-                text += self.tree[codeword]
-                codeword = ''
-        return text
+def decoder(bitstream,tree):
+    '''
+        Let's reverse tree and make easier access to chars
+        For each codeword recognized, get its char in tree
+        and concatenate to text
+    '''
+    if not isinstance(bitstream,str):
+        raise ValueError('Bitstream must be a string')
+    if not isinstance(tree,dict):
+        raise ValueError('Tree must be a dict')
+    tree = dict((value,key) for key,value in tree.items())
+    text = ''
+    codeword = ''
+    for bit in bitstream:
+        codeword += bit
+        if codeword in tree.keys():
+            text += tree[codeword]
+            codeword = ''
+    return text
