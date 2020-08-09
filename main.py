c9655b2cba0c8041a92874dbbd4ad1ec012a87c9
hedo95 <44504802+hedo95@users.noreply.github.com>
2020-08-09 18:59:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,33 @@ from lib.coder import *
 from lib.decoder import *
 
 
-text = """ 
-Packages are a way of structuring Python's module namespace by using “dotted module names”. 
-For example, the module name A.B designates a submodule named B in a package named A. 
-Just like the use of modules saves the authors of different modules from having to worry about each other's global variable names, 
-the use of dotted module names saves the authors of multi-module packages like NumPy or Pillow from having to worry about each other's module names.
-Suppose you want to design a collection of modules (a “package”) for the uniform handling of sound files and sound data. 
-There are many different sound file formats (usually recognized by their extension, for example: .wav, .aiff, .au) 
-so you may need to create and maintain a growing collection of modules for the conversion between the various file formats. 
-There are also many different operations you might want to perform on sound data (such as mixing, adding echo, 
-applying an equalizer function, creating an artificial stereo effect),
-so in addition you will be writing a never-ending stream of modules to perform these operations. 
-"""
+'''
 
-coder = Coder(text)
-tree = coder.binarytree()
-bitstream,tree,Bytes = coder.bitstream(tree=tree)
-print('\n'+bitstream)
-print('\nBytes: '+str(Bytes))
+    There are so many ways to encode a text, here we'll see 2 of them
+    Suggested alternatives could be: arithmetic coding, LZ 77, LZ 78, LZW...
 
-decoder = Decoder(bitstream,tree)
-print('\nDecoded text: '+ decoder.text())
+    Main program 
+'''
+
+Text = str(input('Enter a text to code:\n'))
+
+print(Text)
+
+encoded1_text,size1,tree1 = coder(Text,treetype='complete')
+encoded2_text,size2,tree2 = coder(Text,treetype='full')
+
+print('\nBitstream complete tree: \n\n'+encoded1_text)
+print('\nBytes: '+str(size1))
+
+print('\nBitstream full tree: \n\n'+encoded2_text)
+print('\nBytes: '+str(size2))
+
+'''
+    Now let's decode the text and check if decoder works as well as coder
+'''
+
+decoded1_text = decoder(encoded1_text,tree1)
+print('\nDecoded text1 with decoder: \n'+ decoded1_text)
+
+decoded2_text = decoder(encoded2_text,tree2)
+print('\nDecoded text2 with decoder: \n'+ decoded2_text)
